d8e3a3450dd0f3b4e967a14bb31ae28ef73085d9
decon-harsh <harshsingh9471@gmail.com>
2021-06-06 00:35:38 +0530

    Server and Client

---
+++ client.py
@@ -0,0 +1,125 @@
+import socket
+import sys
+import select
+import pickle
+
+def list_rooms(rooms_list):
+    if len(rooms_list)!= 0:
+        print("------------------------")
+        for keys in rooms_list:
+            print(f"Room No : {keys}")
+        print("------------------------")
+        print("")
+    else:
+        print("No rooms availiable")
+        print("")        
+            
+
+
+def main():
+    port = 12344
+    host = '127.0.0.1'
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host,port))
+
+    # Name
+    while True:
+        name = input("Hey what's your good name: ")
+        if(len(name)!=0):
+            break
+        else:
+            print("Please provide your name to start")
+
+    s.send(bytes(name,'utf-8'))
+
+    salute_message  = s.recv(1024).decode()
+    print(salute_message)
+    print("")
+
+
+    # Options and rooms
+    while True:
+        print("Please choose one of these choices!")
+        print("1: List all the availiable rooms")
+        print("2: Join a room")
+        print("3: Create a room")
+        print("4: Exit")
+
+        choice1 = input("Your Choice: ")
+
+        # List all the choices
+        if(choice1 =="1"):
+            s.send(bytes("LIST",'utf-8'))
+            data_loaded = s.recv(1024)
+            try:
+                rooms_list = pickle.loads(data_loaded)
+            except EOFError:
+                rooms_list = {}
+            list_rooms(rooms_list)
+        
+        # Join a room 
+        elif choice1=="2":
+            room_number = input("Room Number: ")
+            s.send(bytes("JOIN",'utf-8'))
+            s.send(bytes(room_number,'utf-8'))
+
+            success = s.recv(1024).decode()
+            print("")
+            if success=="True":
+                print(f"Joining Room No : {room_number}, DAATEBAYO!") 
+                print("")
+                
+                welcome_message  = s.recv(1024).decode()
+                print(welcome_message)
+                print("")
+
+        
+                while True:
+                    sockets_list = [sys.stdin, s] 
+                    read_sockets, write_socket, error_socket = select.select(sockets_list,[],[])
+                    
+                    for socks in read_sockets:
+                        if socks == s:
+                            message  = socks.recv(1024).decode()
+                            sys.stdout.write(message)
+                            print("")
+                            sys.stdout.flush()
+
+                        else: 
+                            message = sys.stdin.readline()
+                            if message.strip() == '/q':
+                                    print("**BBYE**")
+                                    quit()
+
+                            else:
+                                s.send(bytes(message,'utf-8'))  
+                            print("")    
+                            sys.stdout.flush()
+                    
+                s.close()
+            else:
+                print("Enter a valid Room No, DATTEBAYO!")
+                print("")
+
+
+        # Create a new room
+        elif choice1=="3":
+            s.send(bytes("CREATE",'utf-8'))
+            new_room_num = s.recv(1024).decode()
+            print(f"Room Number {new_room_num} created successfully")
+            print("")
+
+        # Quit 
+        elif choice1=="4":
+            print("As you like it!")
+            quit()
+
+        else:
+            print("Enter a valid choice!")
+            print("")
+            s.send(bytes("CONTINUE",'utf-8'))
+    
+
+
+if __name__ == '__main__':
+    main()
---
+++ server.py
@@ -0,0 +1,141 @@
+from os import remove
+import socket
+from _thread import *
+import threading
+import pickle
+
+# Globally storing in main thread heap
+list_of_clients = {}
+room_list={}
+
+# Function to join a room 
+def join_room(room_number,addr):
+    global room_list 
+
+    if room_number in room_list.keys():
+        room_list[str(room_number)].add(addr)
+        return True
+    else:
+        return False
+    
+# Function to create  a room
+def create_room():
+    global room_list
+    
+    if len(room_list)==0:
+        max_room_num = 999
+    else:
+        max_room_num = max(list(room_list.keys()))
+
+    # Declaring an empty set, just for key creation
+    room_list[str(int(max_room_num)+1)] = set()
+    return str(int(max_room_num)+1)
+
+# Thread
+def multi_threaded_client(c,addr):
+    name_data = c.recv(1024).decode()
+    global room_list
+    
+    c.send(bytes(f"Hey {name_data} What's up?",'utf-8'))
+    
+    while True:
+        try:
+            data = c.recv(1024).decode()
+            
+            # TODO
+
+                        
+            if data and data=="JOIN":
+                global room_list
+
+                room_number = c.recv(1024).decode()
+                success = join_room(room_number,addr)
+                c.send(bytes(str(success),'utf-8'))
+                
+                if success == True:
+                    c.send(bytes(f"Welcome to chatroom {room_number}! {name_data}",'utf-8'))
+
+                    while True:
+                        try:
+                            data = c.recv(5000).decode()
+                
+                            if not data:
+                                remove(addr,room_number)
+
+                            else:
+                                message_to_send = f"<{name_data}>$ {data}"
+
+                                # This function broadcasts message to everyone in the room except the sender
+                                broadcast(message_to_send,c,room_number,addr)
+                
+                        except error as e:
+                            print(e)
+                            remove(addr,room_number)
+                            break    
+
+            elif data and data=="LIST":
+                global room_list
+                data_string = pickle.dumps(room_list) 
+                c.send(bytes(data_string))
+            
+            elif data and data == "CREATE":
+                new_room_num = create_room()
+                c.send(bytes(new_room_num,'utf-8'))
+            
+            else:
+                threading.Lock().release()
+                break
+        
+        except error as e:
+            print(e)
+            # Release lock at last
+            threading.Lock().release()
+
+# Helper Functions
+def broadcast(message,c,room_number,addr):
+    global room_list
+    global list_of_clients
+    for clients in room_list[str(room_number)]:
+        if clients != addr:
+            try:
+                c_object = list_of_clients[clients]
+                c_object.send(bytes(message,'utf-8'))
+            except:
+                c_object = list_of_clients[clients]
+                c_object.close()
+                remove(clients,room_number)
+
+def remove(addr,room_number):
+    global room_list
+    room_list[str(room_number)].discard(addr)
+
+
+def main():
+    port = 12344
+    host = '127.0.0.1'
+    ThreadCount = 0
+    
+    # Server socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # This is for reusing same port after keyboard interupt
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
+
+    try:
+        s.bind((host, port))  #bind
+    except socket.error as e:
+        print(str(e))
+    s.listen(15)
+
+    while True:
+        c,addr =  s.accept()
+        threading.Lock().acquire()
+        print(f"Connected to {addr}")
+        list_of_clients[addr]=c
+        start_new_thread(multi_threaded_client,(c,addr))
+        ThreadCount += 1
+
+    c.close()
+    s.close()    
+
+if __name__ == '__main__':
+    main()
